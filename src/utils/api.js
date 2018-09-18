ca98cee7340af981527b297f2a3564c1a31e0706
18676342166 <1832054053@qq.com>
2018-09-18 19:35:05 +0800

    axios封装

---
+++ src/utils/api.js
@@ -0,0 +1,7 @@
+export default {
+  right: '/api-sensor/constant-data', // 正确路径
+  error: '/topc/5433d5e4e737cbe96dcef312', // url错误
+  backEnd: '/topic/5433d5e4e737cbe96dcef31', // 参数错误
+  getVerifyCode: '/home/get-verify-code', // 获取验证码
+  login: '/home/login'
+}
